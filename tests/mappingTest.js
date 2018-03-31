24ab704c9ac7c749d5b80404972d7fd14527471b
joehartzell1996 <joehartzell1996@icloud.com>
2018-03-31 09:54:34 -0400

    removed the extra function in the tests

--- tests/mappingTest.js
+++ tests/mappingTest.js
@@ -8,64 +8,60 @@ mappings.set('person-mapping-2', PersonMapping2);
 
 let mapper = new Mapper(mappings);
 
-let tests = function(){
-    let test1 = function(){
-        let payload = {
+let test1 = function(){
+    let payload = {
+        _id: 1,
+        'first-name': 'Joe',
+        'last-name': 'Hartzell',
+        'person-address': {
             _id: 1,
-            'first-name': 'Joe',
-            'last-name': 'Hartzell',
-            'person-address': {
+            'address-line-1': 'line 1 address',
+            'address-line-2': 'some state, 14567',
+            'address-state': {
                 _id: 1,
-                'address-line-1': 'line 1 address',
-                'address-line-2': 'some state, 14567',
-                'address-state': {
-                    _id: 1,
-                    abbr: 'PA',
-                    'state-name': 'Pennsylvania'
-                }
+                abbr: 'PA',
+                'state-name': 'Pennsylvania'
             }
         }
-        
-        let mappedPayload = mapper.map(payload, 'person');
-        console.log(mappedPayload);
     }
+    
+    let mappedPayload = mapper.map(payload, 'person');
+    console.log(mappedPayload);
+}
 
-    let test2 = function(){
-        let payload = {
+let test2 = function(){
+    let payload = {
+        _id: 1,
+        'first-name': 'Laura',
+        'last-name': 'Gearhart',
+        'person-addresses': [{
             _id: 1,
-            'first-name': 'Laura',
-            'last-name': 'Gearhart',
-            'person-addresses': [{
+            'address-line-1': 'line 1 address',
+            'address-line-2': 'some state, 14567',
+            'address-state': {
                 _id: 1,
-                'address-line-1': 'line 1 address',
-                'address-line-2': 'some state, 14567',
-                'address-state': {
-                    _id: 1,
-                    abbr: 'PA',
-                    'state-name': 'Pennsylvania'
-                }
-            },
-            {
-                _id: 2,
-                'address-line-1': 'line 1 address',
-                'address-line-2': 'some state, 14567',
-                'address-state': {
-                    _id: 1,
-                    abbr: 'PA',
-                    'state-name': 'Pennsylvania'
-                }
-            }]
-        };
+                abbr: 'PA',
+                'state-name': 'Pennsylvania'
+            }
+        },
+        {
+            _id: 2,
+            'address-line-1': 'line 1 address',
+            'address-line-2': 'some state, 14567',
+            'address-state': {
+                _id: 1,
+                abbr: 'PA',
+                'state-name': 'Pennsylvania'
+            }
+        }]
+    };
 
-        let mappedPayload = mapper.map(payload, 'person-mapping-2');
-        console.log(mappedPayload);
-    }
+    let mappedPayload = mapper.map(payload, 'person-mapping-2');
+    console.log(mappedPayload);
+}
 
-    test1();
-    test2();
-};
-
-tests();
+test1();
+test2();
 
 
 
